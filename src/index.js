b2005e82813495f416654e46b34e9f1c6d120111
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2024-11-29 14:33:43 +1300

    Add floating menu [hidden]

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import closeFolder from './images/svg/file-folder.svg';
 
 const btn = document.getElementById("btn-whatodo");
 const img = btn.querySelector("img");
+const menu = document.getElementById('menu');
 
 btn.addEventListener('click', () => {
     if (img.alt.includes("closed")) {
@@ -13,4 +14,20 @@ btn.addEventListener('click', () => {
         img.src = closeFolder;
         img.alt = "closed folder";
     }
+
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden'); 
+        menu.classList.add('visible');
+    } else {
+        menu.classList.remove('visible');
+        menu.classList.add('hidden');
+    }
 });
+
+
+const animate = function() {
+    img.style.transition = 'transform 0.3s ease-in-out';
+}
+
+
+requestAnimationFrame(animate);
