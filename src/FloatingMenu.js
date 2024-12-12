1b1c447f04e02cbaf68d96ecf88d545fe34efe9e
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2024-12-12 15:38:43 +1300

    Lazy load floating menu

--- src/FloatingMenu.js
+++ src/FloatingMenu.js
@@ -1,35 +1,3 @@
-// // import openFolder from './images/svg/open-file-folder.svg';
-// // import closeFolder from './images/svg/file-folder.svg';
-
-// // const btn = document.getElementById("btn-whatodo");
-// // const img = btn.querySelector("img");
-// // const menu = document.getElementById('menu');
-
-// // btn.addEventListener('click', () => {
-// //     if (img.alt.includes("closed")) {
-// //         img.src = openFolder;
-// //         img.alt = "opened folder";
-// //     } else {
-// //         img.src = closeFolder;
-// //         img.alt = "closed folder";
-// //     }
-
-// //     if (menu.classList.contains('hidden')) {
-// //         menu.classList.remove('hidden'); 
-// //         menu.classList.add('visible');
-// //     } else {
-// //         menu.classList.remove('visible');
-// //         menu.classList.add('hidden');
-// //     }
-// // });
-
-
-// // const animate = function() {
-// //     img.style.transition = 'transform 0.3s ease-in-out';
-// // }
-
-
-// // requestAnimationFrame(animate);
 
 // // const zenQuotesApi ="/quotes";
 
@@ -44,9 +12,9 @@
 // // getZenQuotes(zenQuotesApi);
 
 
-export function FloatingMenu() {
+export default function FloatingMenu() {
     return (
-        <div id="menu" className="hidden">
+        <div id="menu">
             <ul>
                 <li><a href="#">Want a Zen Quote?</a></li>
             </ul>
--- src/FolderButton.js
+++ src/FolderButton.js
@@ -1,9 +1,35 @@
+import { lazy, useState, startTransition } from "react";
+
 import closeFolder from "./images/svg/file-folder.svg";
+import openFolder from "./images/svg/open-file-folder.svg";
+
+const FloatingMenu = lazy(() => import('./FloatingMenu'));
 
 export function FolderButton() {
+
+    const [btnImage, setBtnImage] = useState(closeFolder);
+    const [btnAlt, setBtnAlt] = useState('closed folder');
+
+    function handleClick() {
+        startTransition(() => {
+            console.log('clicked!');
+            if(btnAlt.includes('closed')) {
+                setBtnImage(openFolder);
+                setBtnAlt('opened folder');
+            } else {
+                setBtnImage(closeFolder);
+                setBtnAlt('closed folder');
+            }
+        });
+    }
+
     return (
-        <button type="button" aria-label="Choose what to do" id="btn-whatodo" className="hidden">
-            <img src={closeFolder} alt="closed folder" />
-        </button>
+        <>
+            {console.log(btnAlt.includes('opened'))}
+            {btnAlt.includes('opened') && <FloatingMenu />}
+            <button type="button" aria-label="Choose what to do" id="btn-whatodo" onClick={handleClick} className="hidden">
+                <img src={btnImage} alt={btnAlt} />
+            </button>
+        </>
     );
 }
